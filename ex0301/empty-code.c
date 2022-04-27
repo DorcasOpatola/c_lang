389e131ae2af38095eaf72ea2132b91dbea0407d
Dokie12196$ <preciousopal.od@gmail.com>
2022-04-27 16:29:36 +0100

    use stdio.h header file to declare printf function

--- ex0301/empty-code.c
+++ ex0301/empty-code.c
@@ -1,1 +1,12 @@
-main() {}
+#include <stdio.h>
+
+int main()
+/* int main(): declaring the main() function to be an integer function.
+ *
+ * The int tells the compiler that main() returns an integer value.
+ */
+
+{
+	printf("4 times 5 is %d\n", 4*5);
+	return (0); /* Ends the main() function and therefore the program. */
+}
