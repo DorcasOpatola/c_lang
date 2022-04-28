3020e477da6a0334771b70722447737ba7de7f12
Dokie12196$ <preciousopal.od@gmail.com>
2022-04-28 14:25:55 +0100

    using puts fuunction to print text on a new line

---
+++ ex0403/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * @puts(): is a function that sends text to standard output,
+ * it also prints the text on a new line.
+ */
+int main()
+{
+	puts("Hickory, dickory, dock,");
+	puts("The mouse ran up the clock.");
+	puts("The clock struck one,");
+	puts("The mouse ran down,");
+	puts("Hickory, dickory, dock.");
+	return(0);
+}
