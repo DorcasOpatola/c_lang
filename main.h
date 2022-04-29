96f7add6583cf6afd5825c5f597bcd8c287b5a7d
Dokie12196$ <preciousopal.od@gmail.com>
2022-04-29 17:44:26 +0100

    updates

---
+++ main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>   //Header file for the following functions
+
+/**
+* This is the format for printf() function
+*/
+int printf(const char *restrict format, ...);
+
+/**
+* This is the format for puts() function
+*/
+int puts(const char *s);
