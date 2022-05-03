435ef3cbc8d346aabd57ac1a0caaf1e6ab6229a2
Dokie12196$ <preciousopal.od@gmail.com>
2022-05-03 12:36:06 +0100

    %d does not work for 16.0

---
+++ ex05078/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Always Remember!
+ * When a float value is used for calculation,
+ * %f character specifier should be used.
+ */
+int main()
+{
+	printf("The total is %f.\n", 16.0+17);
+	return(0);
+}
