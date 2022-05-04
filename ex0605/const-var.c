60f01780eedb6f0dd1e48e0d4f99dab8de07d683
Dokie12196$ <preciousopal.od@gmail.com>
2022-05-04 17:09:57 +0100

    declaring and using variables and CONSTANTS

---
+++ ex0605/const-var.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define GLORKUS 16
+
+int main()
+{
+	printf("GLORKUS is a constant integer whose value is %d.\n", GLORKUS);
+	printf("The value of GLORKUS plus 16 is %d.\n", GLORKUS+16);
+	printf("GLORKUS multiplied by itself gives %d.\n", GLORKUS*GLORKUS);
+	return (0);
+}
+
---
+++ ex0605/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int blorf;
+
+	blorf = 22;
+
+	printf("Blorf is an integer whose value is %d.\n", blorf);
+	printf("Blorf plus 16 gives %d.\n", blorf+16);
+	printf("The value of blorf multiplied by itself is %d.\n", blorf*blorf);
+	return (0);
+}
