fa7c8ac591006166873eb3dcd75ba1d9ab123927
Dokie12196$ <preciousopal.od@gmail.com>
2022-05-04 19:20:43 +0100

    variables

---
+++ ex0612/float_var.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	float a = 5.43;
+	float b = 2.17;
+	float c = a + b;
+
+	printf("Float type variable c = %f\n",c);
+	return(0);
+}
---
+++ ex0612/var_mix-up.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 5;
+	int b = 7;
+	int c = a + b;
+	
+	printf("Variable c = %d\n", c);
+	return (0);
+}
