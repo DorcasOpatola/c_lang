99b23f408336ba0799e3989cbc91ca685c825046
Dokie12196$ <preciousopal.od@gmail.com>
2022-05-04 17:59:00 +0100

    variables declaration

---
+++ ex0608/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int shadrach, meshach, abednego;
+
+	shadrach = 10;
+	meshach = 23;
+	abednego = 75;
+	printf("Shadrach is %d\nMeshach is %d\nAbednego is %d\n", shadrach,meshach,abednego);
+	return (0);
+}
