364e5979aeb2a0e25baea126d63a9338dc3d4f40
Dokie12196$ <preciousopal.od@gmail.com>
2022-05-07 20:16:18 +0100

    putchar = one character

---
+++ ex0705/using-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int ch;
+
+	printf("Press Enter: ");
+	getchar();
+	ch = 'H';
+	putchar(ch);
+	ch = 'i';
+	putchar(ch);
+	putchar('!');
+	printf("\n");
+	return (0);
+}
